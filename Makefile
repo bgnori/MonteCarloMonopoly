baa09c7c1d3b74e44bbd31faec63233c776648ce
bgnori <bgnori@gmail.com>
2013-06-15 21:06:54 +0900

    moved Runner execution to main 	modified:   Makefile 	modified:   main.py

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@
 clean:
 	rm *pyc; rm evoke.png
 
-
 run:
+	python main.py > log
+
+test:
 	python montecarlo.py > log
 
 png:
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import sys
 
 
 
 
 
+t = int(sys.args[1])
 
+with file('result.txt', 'w') as f:
+  r = Runner(1, t)
+  r.run()

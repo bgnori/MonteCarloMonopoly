1c84db4af6b483c3742e9a0ed5533007f86ee685
Noriyuki Hosaka <bgnori@gmail.com>
2013-06-20 11:43:46 +0900

    follow up pypoly change 	modified:   graphcmd.py

--- graphcmd.py
+++ graphcmd.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import model
-import command
-import board
-import Atlantic2008
+from pypoly import models
+from pypoly import commands
+from pypoly import board
+from pypoly import Atlantic2008
 
 
 class CallGraphMaker(object):
@@ -14,7 +14,7 @@ class CallGraphMaker(object):
   def scan_module(self, m):
     self.scanned.append(m)
     for k, v in m.__dict__.iteritems():
-      if isinstance(v, type) and issubclass(v, model.Command):
+      if isinstance(v, type) and issubclass(v, models.Command):
         self.knowns[k] = (v, set())
 
   def scan_place(self, xs):
@@ -47,8 +47,8 @@ class CallGraphMaker(object):
 
 cgm = CallGraphMaker()
 
-cgm.scan_module(model)
-cgm.scan_module(command)
+cgm.scan_module(models)
+cgm.scan_module(commands)
 cgm.scan_module(board)
 cgm.scan_module(Atlantic2008)
 cgm.scan_place(Atlantic2008.myPlace.xs)
